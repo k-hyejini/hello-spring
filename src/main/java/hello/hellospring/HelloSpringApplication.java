ddb4af048f46020bd19bb66764092d356ea28fe2
Hyejin <khyejin0813@gmail.com>
2023-08-15 22:40:32 +0900

    Spring Boot의 main 메소드 설명 추가 (내장된 tomcat을 띄우면서 Spring Boot 실행)

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
     public static void main(String[] args) {
+        // 내장된 tomcat을 띄우면서 Spring Boot 실행
         SpringApplication.run(HelloSpringApplication.class, args);
     }
 
